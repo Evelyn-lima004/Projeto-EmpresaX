6180a4042101b0f18f5e26fa52e2550a1a8e773b
EVELYN LAVIGNE GOMES DE LIMA <21193113@CFP127ALUNO.LOCAL>
2021-09-09 11:22:42 -0300

    atualizar

--- funcoes.php
+++ funcoes.php
@@ -1,34 +1,82 @@
 <?php
 
-//recebe o nome do arquivo
 function lerArquivo($nomeArquivo){
-    //lê o arquivo com string
-   $arquivo = file_get_contents($nomeArquivo);
-   // transforma a string em array
-   $jsonArray = json_decode($arquivo);
-   // devolve o array
-   return $jsonArray;
+
+    $arquivo = file_get_contents($nomeArquivo);
+
+    $arquivoArr = json_decode($arquivo);
+
+    return $arquivoArr;
+
 }
+
+function buscarFuncionario($funcionarios, $filtro){
+
+    $funcionariosFiltro = [];
+
+    foreach($funcionarios as $funcionario){
+        if(
+            strpos($funcionario->first_name, $filtro) !== false
+            || 
+            strpos($funcionario->last_name, $filtro) !== false
+            ||
+            strpos($funcionario->department, $filtro) !== false
+        ){
+            $funcionariosFiltro[] = $funcionario;
+        }
+    }
+
+    return $funcionariosFiltro;
+
+}
+
+
+//adicionando o funcionario
+function adicionarFuncionario($nomeArquivo, $novoFuncionario){
+
+    $funcionarios = lerArquivo($nomeArquivo);
+
+    $funcionario[] = $novoFuncionario;
+
+    $json = json_encode($funcionarios);
+
+    file_put_contents($nomeArquivo, $json);
+  
+
+    
+}
+
+//recebe o nome do arquivo
+// function lerArquivo($nomeArquivo){
+//     //lê o arquivo com string
+//    $arquivo = file_get_contents($nomeArquivo);
+//    // transforma a string em array
+//    $jsonArray = json_decode($arquivo);
+//    // devolve o array
+//    return $jsonArray;
+// }
 
 // busca o funcionario dentro da lista e devolve uma lisa com os funcionarios encontrados
-function buscarFuncionarios($funcionarios, $nome){
+// function buscarFuncionarios($funcionarios, $nome){
 
-    $funcionariosFiltro = [];
-    foreach($funcionarios as $funcionario){
-        if ($funcionario->first_name == $nome){
-            $funcionariosFiltro[] = $funcionario;
-        }
-    }
-    return $funcionariosFiltro;
-}
+//     $funcionariosFiltro = [];
+//     foreach($funcionarios as $funcionario){
+//         if ($funcionario->first_name == $nome){
+//             $funcionariosFiltro[] = $funcionario;
+//         }
+//     }
+//     return $funcionariosFiltro;
+// }
 
-function buscarDepartamento($funcionarios, $nome){
+// busca o departamento dentro da lista e devolve uma lisa com os funcionarios encontrados
+// function search($funcionarios, $findme){
 
-    $funcionariosFiltro = [];
-    foreach($funcionarios as $funcionario){
-        if ($funcionario->first_name == $nome){
-            $funcionariosFiltro[] = $funcionario;
-        }
-    }
-    return $funcionariosFiltro;
-}
+//     $funcionariosFiltro = [];
+//     foreach($funcionarios as $funcionario){
+//         if ($funcionario->findme == $findme){
+//             $funcionariosFiltro[] = $funcionario;
+//         }
+//     }
+//     return $funcionariosFiltro;
+// }
+
--- index.php
+++ index.php
@@ -2,57 +2,115 @@
 
 require("./funcoes.php");
 
-$funcionarios = lerArquivo("funcionarios.json");
+$funcionarios = lerArquivo("./dados/funcionarios.json");
 
-if(isset($_GET["buscarFuncionario"])){
-   $funcionarios = buscarFuncionarios($funcionarios, $_GET["buscarFuncionario"]);
+if(isset($_GET["filtro"]) && $_GET["filtro"] != ""){
+  $funcionarios = buscarFuncionario($funcionarios, $_GET["filtro"]);
 }
 
 ?>
-
-
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="styleeshet" href="./style.css"/>
-    <title>EmpresaX</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./style.css" />
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <title>Empresa X</title>
 </head>
+
 <body>
-<form>
-        <input type="text" value= "<?= isset($_GET["buscarFuncionario"]) ? $_GET["buscarFuncionario"] :"" ?>"
-        name="buscarFuncionario"placeholder="Buscar Aluno" />
-        <button>Buscar</button>
+  <div class="content">
+    <h1>Funcionários da empresa X</h1>
+    <p style="text-align: center">
+      A empresa conta com <em> <?= count($funcionarios) ?> </em> funcionários.
+    </p>
+    <form method="GET" class="search-form">
+      <div class="input-group" style="flex: 1">
+        <label>Pesquisar por nome</label>
+        <input type="search" 
+          placeholder="Digite o nome" 
+          name="filtro" 
+          value="<?= isset($_GET["filtro"]) ? $_GET["filtro"] : "" ?>" />
+      </div>
+      <button class="material-icons">
+        person_search
+      </button>
     </form>
-    <table border="1">
+    <form id="form-funcionario" action="acoes.php" method="POST">
+        <h1>Adicionar funcionário</h1>
+        <input type="text" placeholder="Digite o id" name="id" />
+        <input type="text" placeholder="Digite o primeiro nome" name="first_name" />
+        <input type="text" placeholder="Digite o sobrenome" name="last_name" />
+        <input type="text" placeholder="Digite o e-mail" name="email" />
+        <input type="text" placeholder="Digite o sexo" name="gender" />
+        <input type="text" placeholder="Digite o IP" name="ip_address" />
+        <input type="text" placeholder="Digite o país" name="country" />
+        <input type="text" placeholder="Digite o departamento" name="department" />
+        <button>Salvar</button>
+    </form>
+    <table>
+      <tr>
+        <th>
+          ID
+        </th>
+        <th>
+          Nome
+        </th>
+        <th>
+          Sobrenome
+        </th>
+        <th>
+          E-mail
+        </th>
+        <th>
+          Sexo
+        </th>
+        <th>
+          Endereço IP
+        </th>
+        <th>
+          País
+        </th>
+        <th>
+          Departamento
+        </th>
+      </tr>
+      <?php
+      foreach ($funcionarios as $funcionario) :
+      ?>
         <tr>
-            <th>Id</th>
-            <th>Nome</th>
-            <th>Sobrenome</th>
-            <th>Email</th>
-            <th>Sexo</th>
-            <th>Endereço Ip</th>
-            <th>País</th>
-            <th>Departamento</th>
+          <td>
+            <?= $funcionario->id ?>
+          </td>
+          <td>
+            <?= $funcionario->first_name ?>
+          </td>
+          <td>
+            <?= $funcionario->last_name ?>
+          </td>
+          <td>
+            <?= $funcionario->email ?>
+          </td>
+          <td>
+            <?= $funcionario->gender ?>
+          </td>
+          <td>
+            <?= $funcionario->ip_address ?>
+          </td>
+          <td>
+            <?= $funcionario->country ?>
+          </td>
+          <td>
+            <?= $funcionario->department ?>
+          </td>
         </tr>
-        <?php
-        foreach ($funcionarios as $funcionario) :
-        ?> 
-           <tr>
-               <td><?= $funcionario->id; ?></td>
-               <td><?= $funcionario->first_name; ?></td>
-               <td><?= $funcionario->last_name; ?></td>
-               <td><?= $funcionario->email; ?></td>
-               <td><?= $funcionario->gender; ?></td>
-               <td><?= $funcionario->ip_address; ?></td>
-               <td><?= $funcionario->country; ?></td>
-               <td><?= $funcionario->department; ?></td>
-           </tr>
-        <?php
-        endforeach;
-        ?>
+      <?php
+      endforeach;
+      ?>
     </table>
+  </div>
 </body>
+
 </html>
